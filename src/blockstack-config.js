d599c2482f06cd4b2e4d2ad1eeb2579bdb7731d9
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-08-25 18:32:21 +0300

    checkpoint

---
+++ src/blockstack-config.js
@@ -0,0 +1,27 @@
+import {UserSession, AppConfig} from 'blockstack';
+import {configure} from 'radiks';
+import {decodeToken} from 'jsontokens'
+
+const RADIKS_URL = `https://${window.location.protocol === 'http:' ? 'radiks-dev.dhost.me' : 'radiks.dhost.me'}`;
+
+const domain = window.location.origin;
+
+export const userSession = new UserSession({
+  appConfig: new AppConfig(['store_write', 'publish_data'], domain, '/app/auth', '/manifest.json')
+});
+
+configure({
+  apiServer: RADIKS_URL,
+  userSession
+});
+
+
+export const decodeUserResponseToken = () => {
+  const {authResponseToken} = userSession.loadUserData();
+  return decodeToken(authResponseToken);
+};
+
+export const getUsername = () => {
+  const {username} = decodeUserResponseToken().payload;
+  return username;
+};
--- src/containers/home/page.js
+++ src/containers/home/page.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
+
+import {userSession} from '../../blockstack-config';
 
 class HomePage extends Component {
+
+  signIn = (e) => {
+    e.preventDefault();
+
+    if (userSession.isUserSignedIn()) {
+      const {history} = this.props;
+      history.push('/app/editor');
+      return;
+    }
+
+    userSession.redirectToSignIn();
+  };
+
   render() {
     return (
-      <span>Hello</span>
+      <p className="text-center" style={{padding: '20px'}}><Button onClick={this.signIn}>Sign In</Button></p>
     )
   }
 }
