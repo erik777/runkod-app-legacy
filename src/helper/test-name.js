52d60fa9ec670674164f2a71115cb6cc4951ab52
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-10-19 15:35:57 +0300

    corrections and improvements

--- src/helper/test-name.js
+++ src/helper/test-name.js
@@ -1,5 +1,5 @@
 import {NAME_MAX_LENGTH} from "../constants";
 
 export default (s) => {
-  return s.trim() !== '' && s.trim().length <= NAME_MAX_LENGTH && /^[a-z][a-z0-9]*$/i.test(s);
+  return s.trim() !== '' && s.trim().length <= NAME_MAX_LENGTH && /^[a-z][a-z0-9-]*$/i.test(s);
 };
