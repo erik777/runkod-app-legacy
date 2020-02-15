528cd8407d13ed179484c02738bfc2790dbd7453
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-02-15 10:41:19 +0300

    adds snapshot tests for new project forms

---
+++ src/components/dialogs/new-project/form-custom.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ProjectForm from './form-custom';
+import TestRenderer from 'react-test-renderer';
+
+
+test('1- default', () => {
+  const props = {
+    switchFn: () => {
+    },
+    onDone: () => {
+    }
+  };
+
+  const renderer = TestRenderer.create(
+    <ProjectForm {...props}/>
+  );
+
+  expect(renderer.toJSON()).toMatchSnapshot();
+});
